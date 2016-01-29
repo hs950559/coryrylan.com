ad35c079af477611974b9ce1609073daa1f0ef35
Cory Bateman <splintercode.cb@gmail.com>
2016-01-28 21:28:03 -0600

    service worker offline support

--- service-worker.js
+++ service-worker.js
@@ -1,66 +1,51 @@
-var VERSION = 'v1.0.0';
+'use strict';
+
+var VERSION = 'v1.0.6';
+const DEBUG = false;
 
 console.log('WORKER: Version ' + VERSION);
 
 self.addEventListener('install', event => {
-  console.log('WORKER: install event in progress.');
-  
-  event.waitUntil(caches.open(VERSION).then(cache => {
-        return cache.addAll([
-          '/'
-        ]);
-      }).then(() => {
-        console.log('WORKER: install completed');
-      })
-  );
+    log('WORKER: install event in progress.');
+    loadCache(event);
 });
 
 self.addEventListener('fetch', event => {
-  console.log('WORKER: fetch event in progress.');
-
-  if (event.request.method !== 'GET') {
-    console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
-    return;
-  }
-
-  event.respondWith(caches.match(event.request).then(cached => {
-        let networked = fetch(event.request).then(fetchedFromNetwork, unableToResolve).catch(unableToResolve);
-        console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
-        
-        return cached || networked;
-
-        function fetchedFromNetwork(response) {
-          let cacheCopy = response.clone();
-          console.log('WORKER: fetch response from network.', event.request.url);
-
-          caches.open(VERSION + 'pages').then(function add(cache) {
-              cache.put(event.request, cacheCopy);
-            }).then(() => {
-              console.log('WORKER: fetch response stored in cache.', event.request.url);
-            });
-
-          return response;
-        }
-
-        function unableToResolve () {
-          console.log('WORKER: fetch request failed in both cache and network.');
-
-          return new Response('<h1>Service Unavailable</h1>', {
-            status: 503,
-            statusText: 'Service Unavailable',
-            headers: new Headers({
-              'Content-Type': 'text/html'
-            })
-          });
-        }
-      })
-  );
+    log('WORKER: fetch event in progress.');
+    handleRequest(event);
 });
 
 self.addEventListener('activate', event => {
-  console.log('WORKER: activate event in progress.');
+    log('WORKER: activate event in progress.');
+    clearCache(event);
+});
 
-  event.waitUntil(caches.keys().then(keys => {
+
+function handleRequest(event) {
+    if ((event.request.method !== 'GET') || (isHostDomainRequest(event.request.url) && event.request.method !== 'GET')) {
+        log('WORKER: fetch event (GET) ignored.', event.request.method, event.request.url);
+        return;
+    }
+
+    event.respondWith(caches.match(event.request).then(cached => {
+        log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
+        return cached || fetch(event.request).then(response => { storeInCache(response, event) }, unableToResolveRequest).catch(unableToResolveRequest);
+    }));
+}
+
+function loadCache(event) {    
+  event.waitUntil(caches.open(VERSION).then(cache => {
+        return cache.addAll([
+          '/assets/images/cory-rylan.jpg'
+        ]);
+      }).then(() => {
+        log('WORKER: install completed');
+      })
+  );
+}
+
+function clearCache(event) {
+      event.waitUntil(caches.keys().then(keys => {
         return Promise.all(
           keys.filter(key => {
               return !key.startsWith(VERSION);
@@ -70,7 +55,46 @@ self.addEventListener('activate', event => {
             })
         );
       }).then(() => {
-        console.log('WORKER: activate completed.');
+        log('WORKER: activate completed.');
       })
   );
-});
+}
+
+function storeInCache(response, event) {
+    let cacheCopy = response.clone();
+    log('WORKER: fetch response from network.', event.request.url);
+
+    caches.open(VERSION + 'pages').then(function add(cache) {
+        cache.put(event.request, cacheCopy);
+    }).then(() => {
+        log('WORKER: fetch response stored in cache.', event.request.url);
+    });
+
+    return response;
+}
+
+function unableToResolveRequest() {
+    log('WORKER: fetch request failed in both cache and network.');
+
+    return new Response('<h1>Service Unavailable</h1>', {
+        status: 503,
+        statusText: 'Service Unavailable',
+        headers: new Headers({
+            'Content-Type': 'text/html'
+        })
+    });
+}
+
+function isHostDomainRequest(url) {
+    if ((url.indexOf('coryrylan') > -1) || (url.indexOf('localhost') > -1)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function log(msg, url) {
+    if (DEBUG) {
+        console.log(msg, url);
+    }
+}
